aa955973b594ba730d21ae982a3ced2ef5d5af40
RansalaNikethani <u.ransala@gmail.com>
2024-11-21 16:27:53 +0530

    creating an array of 3 objects

---
+++ complexDataTypesArrays.js
@@ -0,0 +1,17 @@
+let books=[
+    {
+        title:"Speaking JavaScript",
+        author:"Axel Rauschmayer",
+        numberOfPages:460
+    },
+    {
+        title:"Programming JavaScript Applications",
+        author:"Eric Elliott",
+        numberOfPages:254
+    },
+    {
+        title:"Understanding ECMAScript 6",
+        author:"Nicholas C. Zakas",
+        numberOfPages:352
+    }
+];
