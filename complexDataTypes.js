72c829468385c66be07da693560bc7d4536346c6
RansalaNikethani <u.ransala@gmail.com>
2024-11-21 16:02:40 +0530

    creating an object and display values

---
+++ complexDataTypes.js
@@ -0,0 +1,9 @@
+let ticket = {
+    from: "Maradana",
+    to: "Pettah",
+    price: 50,
+};
+
+console.log(`Ticket from: ${ticket.from}`);
+console.log(`Ticket to: ${ticket.to}`);
+console.log(`Ticket price: ${ticket.price}`);
