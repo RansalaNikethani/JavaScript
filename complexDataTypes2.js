6b3debc0b4f6be3c152546c68df554bb90813a3d
RansalaNikethani <u.ransala@gmail.com>
2024-11-21 16:17:00 +0530

    declaring empty object and enter values using dot notation

---
+++ complexDataTypes2.js
@@ -0,0 +1,9 @@
+let person = {};
+
+person.name="Ransala";
+person.surname="Nikethani";
+
+/*console.log(person.name);
+console.log(person.surname);*/
+
+console.log('${person.name} ${person.surname}');
