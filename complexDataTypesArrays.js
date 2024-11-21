7fc81c920b1df327371bf0b5a3f88e1c7c85bd24
RansalaNikethani <u.ransala@gmail.com>
2024-11-21 17:21:14 +0530

    contact list array - push,shift,pop,length cmnds used

--- complexDataTypesArrays.js
+++ complexDataTypesArrays.js
@@ -49,3 +49,33 @@ console.log(books[2].title);
 
 let sumOfPages = books[0].numberOfPages + books[1].numberOfPages + books[2].numberOfPages;
 console.log(`pages : ${sumOfPages}`);
+
+let contacts = [{
+    name: "Maxwell Wright",
+    phone: "(0191) 719 6495",
+    email: "Curabitur.egestas.nunc@nonummyac.co.uk"
+    }, {
+    name: "Raja Villarreal",
+    phone: "0866 398 2895",
+    email: "posuere.vulputate@sed.com"
+    }, {
+    name: "Helen Richards",
+    phone: "0800 1111",
+    email: "libero@convallis.edu"
+    }];
+    
+    let newContact = {
+    name: "Maisie Haley",
+    phone: "0913 531 3030 ",
+    email: "risus.Quisque@urna.ca."
+    };
+    
+    contacts.push(newContact);
+    
+    let firstContact = contacts.shift();
+    let lastContact = contacts.pop();
+
+    console.log(firstContact);
+    console.log(lastContact);
+    console.log(contacts.length);
+    
